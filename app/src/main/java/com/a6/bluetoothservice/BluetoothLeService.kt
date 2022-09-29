c44a96a53ba11af7f5fdbbbe7c712b16e57e27ca
Alejandro <alemg77@yahoo.com.ar>
2022-09-29 16:17:13 -0300

    Primer commit

---
+++ app/src/main/java/com/a6/bluetoothservice/BluetoothLeService.kt
@@ -0,0 +1,230 @@
+package com.a6.bluetoothservice
+
+import android.annotation.SuppressLint
+import android.app.Service
+import android.bluetooth.*
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+
+@SuppressLint("MissingPermission")
+class BluetoothLeService() : Service() {
+
+    init {
+        Log.d(TAG, "BluetoothLeService running.... ")
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "BluetoothLeService onCreate() ")
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "BluetoothLeService onDestroy() ")
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    private var bluetoothGatt: BluetoothGatt? = null
+
+    private var connectionState = STATE_DISCONNECTED
+
+    var serviciosGatt: List<BluetoothGattService>? = null
+        private set
+
+    private val gattCallback: BluetoothGattCallback = object : BluetoothGattCallback() {
+
+        /*
+        override fun onCharacteristicChanged(
+            gatt: BluetoothGatt,
+            characteristic: BluetoothGattCharacteristic
+        ) {
+            super.onCharacteristicChanged(gatt, characteristic)
+            Log.d(TAG, "onCharacteristicChanged")
+        }
+
+        override fun onCharacteristicRead(
+            gatt: BluetoothGatt,
+            characteristic: BluetoothGattCharacteristic,
+            status: Int
+        ) {
+            super.onCharacteristicRead(gatt, characteristic, status)
+            Log.d(TAG, "onCharacteristicRead")
+        }
+         */
+
+        override fun onCharacteristicWrite(
+            gatt: BluetoothGatt,
+            characteristic: BluetoothGattCharacteristic,
+            status: Int
+        ) {
+            super.onCharacteristicWrite(gatt, characteristic, status)
+            Log.d(TAG, "onCharacteristicWrite status: $status")
+        }
+
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            super.onConnectionStateChange(gatt, status, newState)
+            Log.d(TAG, "onConnectionStateChange")
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+
+                sendDataToActivity(STATE_CONNECTED)
+
+                connectionState = STATE_CONNECTED
+
+                bluetoothGatt!!.discoverServices()
+
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                connectionState = STATE_DISCONNECTED
+                Log.i(TAG, "Disconnected from GATT server.")
+            }
+        }
+
+        /*
+        override fun onDescriptorRead(
+            gatt: BluetoothGatt,
+            descriptor: BluetoothGattDescriptor,
+            status: Int
+        ) {
+            super.onDescriptorRead(gatt, descriptor, status)
+            Log.d(TAG, "onDescriptorRead")
+        }
+         */
+
+        override fun onDescriptorWrite(
+            gatt: BluetoothGatt,
+            descriptor: BluetoothGattDescriptor,
+            status: Int
+        ) {
+            super.onDescriptorWrite(gatt, descriptor, status)
+            Log.d(TAG, "onDescriptorWrite")
+        }
+
+        override fun onMtuChanged(gatt: BluetoothGatt, mtu: Int, status: Int) {
+            super.onMtuChanged(gatt, mtu, status)
+            Log.d(TAG, "onMtuChanged")
+        }
+
+        override fun onPhyRead(gatt: BluetoothGatt, txPhy: Int, rxPhy: Int, status: Int) {
+            super.onPhyRead(gatt, txPhy, rxPhy, status)
+            Log.d(TAG, "onPhyRead")
+        }
+
+        override fun onPhyUpdate(gatt: BluetoothGatt, txPhy: Int, rxPhy: Int, status: Int) {
+            super.onPhyUpdate(gatt, txPhy, rxPhy, status)
+            Log.d(TAG, "onPhyUpdate")
+        }
+
+        override fun onReadRemoteRssi(gatt: BluetoothGatt, rssi: Int, status: Int) {
+            super.onReadRemoteRssi(gatt, rssi, status)
+            Log.d(TAG, "onReadRemoteRssi")
+        }
+
+        override fun onReliableWriteCompleted(gatt: BluetoothGatt, status: Int) {
+            super.onReliableWriteCompleted(gatt, status)
+            Log.d(TAG, "onReliableWriteCompleted")
+        }
+
+        override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+            super.onServicesDiscovered(gatt, status)
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                Log.d(TAG, "onServicesDiscovered GATT_SUCCESS ")
+                serviciosGatt = gatt.services
+
+                displayGattServices(serviciosGatt)
+
+                sendDataToActivity(STATE_SERVICES_DISCOVERED)
+
+            } else {
+                Log.d(
+                    TAG,
+                    "onServicesDiscovered received: $status"
+                )
+            }
+        }
+    }
+
+    private var mWriteCharacteristic: BluetoothGattCharacteristic? = null
+
+    private fun displayGattServices(gattServices: List<BluetoothGattService>?) {
+
+        if (gattServices == null) return
+
+
+        for (gattService in gattServices) {
+
+            val uuid = gattService.uuid.toString()
+            Log.d(TAG, "gattService: $uuid")
+            val gattCharacteristics = gattService.characteristics
+            for (gattCharacteristic in gattCharacteristics) {
+
+
+                val uuid1 = gattCharacteristic.uuid
+                val properties = gattCharacteristic.properties
+                Log.d(TAG, "    gattCharacteristic: $uuid1")
+
+                if (properties and BluetoothGattCharacteristic.PROPERTY_WRITE != 0x0) {
+                    Log.d(TAG, "        PROPERTY_WRITE")
+                    mWriteCharacteristic = gattCharacteristic
+                }
+
+                if (properties and BluetoothGattCharacteristic.PROPERTY_NOTIFY != 0x0) {
+                    Log.d(TAG, "        PROPERTY_NOTIFY")
+                }
+
+                if (properties and BluetoothGattCharacteristic.PERMISSION_READ != 0x0) {
+                    Log.d(TAG, "        PERMISSION_READ")
+                }
+
+                if (properties and BluetoothGattCharacteristic.PROPERTY_INDICATE != 0x0) {
+                    Log.d(TAG, "        PROPERTY_INDICATE")
+                }
+
+            }
+        }
+    }
+
+    fun conectarGatt(device: BluetoothDevice) {
+        Log.d(TAG, "Iniciando GATT")
+        bluetoothGatt = device.connectGatt(this, false, gattCallback)
+    }
+
+    fun enviar(byteArray: ByteArray): Boolean {
+
+        if (connectionState == STATE_DISCONNECTED) {
+            return false
+        }
+
+        if (mWriteCharacteristic == null) {
+            return false
+        }
+
+        mWriteCharacteristic?.value = byteArray
+
+
+        bluetoothGatt?.writeCharacteristic(mWriteCharacteristic)
+
+        return true
+
+    }
+
+    private val _notifications = MutableLiveData<Int>()
+    val notifications: LiveData<Int> = _notifications
+
+    private fun sendDataToActivity(data: Int) {
+        _notifications.postValue(data)
+    }
+
+    companion object {
+        private const val TAG = "TAGGG_BluetoothLeService"
+        const val STATE_DISCONNECTED = 0
+        const val STATE_CONNECTED = 2
+        const val STATE_SERVICES_DISCOVERED = 3
+    }
+
+}
---
+++ app/src/main/java/com/a6/bluetoothservice/MainActivity.kt
@@ -0,0 +1,240 @@
+package com.a6.bluetoothservice
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val mBluetoothLeService = BluetoothLeService()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "Inicio de la app BluetoothService")
+
+        Intent(this, BluetoothLeService::class.java).also {
+            startService(it)
+        }
+
+        startBroadcastReceiverBluetoothAdapter()
+
+        receiveDataFromBluetoothLeService()
+
+        if (checkMultiplesPermissions(PERMISSIONS)) {
+            if (checkBluetoothEnable()) {
+                checkBondedDevices()
+            }
+        } else {
+            askForMultiplesPermissions(PERMISSIONS)
+        }
+
+    }
+
+    private fun receiveDataFromBluetoothLeService() {
+        mBluetoothLeService.notifications.observe(this) {
+
+            when (it) {
+                BluetoothLeService.STATE_CONNECTED -> {
+                    Log.d(TAG, "El servicio avisa que se conecto ")
+                }
+                BluetoothLeService.STATE_SERVICES_DISCOVERED -> {
+                    Log.d(TAG, "Listo para funcionar!!!!!!!!!!!!!!!!!!!!!!")
+                    val t:String = "pulsador 1  "
+                    mBluetoothLeService.enviar(t.toByteArray())
+                }
+            }
+
+        }
+    }
+
+    override fun onDestroy() {
+
+        Intent(this, BluetoothLeService::class.java).also {
+            stopService(it)
+        }
+
+        stopBroadcastReceiverBluetoothAdapter()
+
+        super.onDestroy()
+    }
+
+    private fun startBroadcastReceiverBluetoothAdapter() {
+        IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED).also {
+            registerReceiver(broadcastReceiverBluetoothAdapter, it)
+        }
+    }
+
+    private fun stopBroadcastReceiverBluetoothAdapter() {
+        unregisterReceiver(broadcastReceiverBluetoothAdapter)
+    }
+
+    private val broadcastReceiverBluetoothAdapter: BroadcastReceiver =
+        object : BroadcastReceiver() {
+
+            override fun onReceive(context: Context, intent: Intent) {
+                val action = intent.action
+
+                // When discovery finds a device
+                if (action == BluetoothAdapter.ACTION_STATE_CHANGED) {
+                    when (intent.getIntExtra(
+                        BluetoothAdapter.EXTRA_STATE,
+                        BluetoothAdapter.ERROR
+                    )) {
+
+                        BluetoothAdapter.STATE_OFF -> {
+                            Log.d(TAG, "onReceive: STATE OFF")
+                        }
+
+                        BluetoothAdapter.STATE_TURNING_OFF -> {
+                            Log.d(TAG, "mBroadcastReceiver1: STATE TURNING OFF")
+                        }
+
+                        BluetoothAdapter.STATE_ON -> {
+                            Log.d(TAG, "mBroadcastReceiver1: STATE ON")
+                        }
+
+                        BluetoothAdapter.STATE_TURNING_ON -> {
+                            Log.d(TAG, "mBroadcastReceiver1: STATE TURNING ON")
+                        }
+                    }
+                }
+            }
+        }
+
+
+    private fun checkMultiplesPermissions(permissions: Array<String>): Boolean {
+
+        for (permission in permissions) {
+            if (!checkPermission(permission)) {
+                return false
+            }
+        }
+        return true
+
+    }
+
+    private fun checkPermission(permission: String): Boolean {
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                permission
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return false
+        }
+        return true
+    }
+
+    private fun askForMultiplesPermissions(permissions: Array<String>) {
+
+        val per = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions())
+        {
+            for (permission in it) {
+                if (!permission.value) {
+                    Log.d(TAG, "Nos rechazaron: $permission")
+                    return@registerForActivityResult
+                }
+            }
+            checkBluetoothEnable()
+        }
+
+        per.launch(permissions)
+
+    }
+
+    private fun checkBluetoothEnable(): Boolean {
+
+        val bluetoothManager = this.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+
+        if (!bluetoothManager.adapter.isEnabled) {
+
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+
+            val resultLauncher =
+                registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                    if (result.resultCode == Activity.RESULT_OK) {
+                        val data: Intent? = result.data
+
+                        // TODO: Verificar que el usuario acepto activar bluetooth
+
+                        checkBondedDevices()
+
+                    }
+                }
+
+            resultLauncher.launch(enableBtIntent)
+
+            return false
+
+        } else {
+            return true
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun checkBondedDevices() {
+
+        val bluetoothManager = this.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+
+        val adapter = bluetoothManager.adapter
+
+        val pairedDevices: Set<BluetoothDevice> = adapter.bondedDevices
+
+        pairedDevices.forEach { device ->
+
+            val deviceName = device.name
+
+            val deviceHardwareAddress = device.address // MAC address
+
+            Log.d(TAG, " $deviceName  $deviceHardwareAddress ")
+
+            if (deviceName.equals("Wireless Controller")) {
+
+                Log.d(TAG, "Encontramos un dispocitivo Wireless Controller")
+
+                mBluetoothLeService.conectarGatt(device)
+
+            }
+
+        }
+    }
+
+    companion object {
+        const val TAG = "TAGGG_MainActivity"
+
+        var PERMISSIONS = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_ADMIN,
+                Manifest.permission.BLUETOOTH_CONNECT
+            )
+        } else {
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_ADMIN
+            )
+        }
+
+    }
+}
